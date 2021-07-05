ec6bbd39520955ffb69266ed3b494db1ab20886e
Md. Feroz Ahmed <ferozahmed.cse26@gmail.com>
2021-07-05 20:12:09 +0600

    Removed log

--- service-etl/src/services/web-scoket.service.js
+++ service-etl/src/services/web-scoket.service.js
@@ -6,8 +6,6 @@ webSocketService = {
         this.wss = new WebSocket.Server({ port: 8081 });
     },
     sendMessage: function (data) {
-        console.log(data);
-        console.log(data, this.wss);
         this.wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(data);
